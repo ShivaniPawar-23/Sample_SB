6b541a41ed305f9399dddec17d4dec41e09a4252
ShivaniPawar-23 <shivanipan6@gmail.com>
2024-11-22 11:57:55 +0530

    first commit

---
+++ src/com/tka/PatternExample.java
@@ -0,0 +1,16 @@
+package com.tka;
+
+public class PatternExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for (int i=0; i<=2; i++) {
+			for (int j=0; j<=3; j++) {
+				System.out.print("*" + " ");
+			}
+			System.out.println();
+		}
+	}
+
+}
